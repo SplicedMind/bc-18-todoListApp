c17da183f5afe406cd0e9def8647b12481cf6337
Akinbode Femi <femi661@gmail.com>
2017-01-19 06:47:46 +0100

    Firebase script added

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var firebase = require('firebase');
-//var bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
+var app = express();
 
-var config = 
+var config =
 {
 	apiKey: "AIzaSyB21pDxjhaLtbVragSySMcqWyIL24i79Xk",
 	authDomain: "todolist-a5040.firebaseapp.com",
@@ -13,9 +14,7 @@ var config =
 
 firebase.initializeApp(config);
 
-var app = express();
-
-//var urlencodedParser = bodyParser.urlencoded({extended: false});
+var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 app.use('/css', express.static('css'));
 app.use('/scripts', express.static('scripts'));
@@ -24,6 +23,9 @@ app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/index', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
 
 app.get('/signup', function(req, res){
 	res.sendFile(__dirname + '/signup.html');
@@ -33,7 +35,21 @@ app.get('/stayorg', function(req, res){
 	res.sendFile(__dirname + '/stayorg.html');
 });
 
+
 app.listen(2020);
+
+function signin()
+{
+	window.alert('SignInCalled');
+	firebase.auth().signInWithEmailAndPassword(email, pword).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+	window.alert(errorCode + ' : '+ errorMessage);
+  // ...
+});
+window.alert('SignInCalled');
+}
 
 /*{
   "rules": {
